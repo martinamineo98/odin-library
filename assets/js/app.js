61138aa5529ae7a54c52a6a6e52cf2ae4efde51b
martinamineo98 <martinamineo98@gmail.com>
2022-08-24 04:43:02 +0200

    Refactored Code

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,197 +1,253 @@
 
 let books = []
 
-function Book(name, author, pages, read) {
-	this.name = name
-	this.author = author
-	this.pages = pages
-	this.read = read
-	this.changeStatus = () => this.read = this.read ? false : true
+class Book {
 	
-	books.push(this)
+	constructor(name, author, pages, status) {
+		this._name = name
+		this._author = author
+		this._pages = pages
+		this._status = status
+	}
+	
+	set name(x) {
+		const str = 'Name Error: Length must be at least 1 character.'
+		this._name = (x === '') ? str : x
+	}
+	
+	get name() {
+		const str = 'Name Error: The Name was never set.'
+		return (this._name === '') ? str : this._name
+	}
+	
+	set author(x) {
+		const str = 'Author Name Error: Length must be at least 1 character.'
+		this._author = (x === '') ? str : x
+	}
+	
+	get author() {
+		const str = 'Author Name Error: The Author Name was never set.'
+		return (this._author === '') ? str : this._author
+	}
+	
+	set pages(x) {
+		const str = 'Page Number Error: The Page Number is not a number.'
+		this._pages = (typeof x !== Number || typeof x.toString() !== Number) ? str : this._pages
+	}
+	
+	get pages() {
+		const str = 'Page Number Error: The Page Number was never set.'
+		return (this._pages === '') ? str : this._pages
+	}
+	
+	set status(x) {
+		const str = 'Read Status Error: The Read Status is not a boolean.'
+		this._status = (typeof x !== Boolean) ? str : this._status
+	}
+	
+	get status() {
+		const str = 'Read Status Error: The Read Status was never set.'
+		return (this._status === '') ? str : this._status
+	}
+	
+	// Change the Read Status of a book.
+	
+	get changeReadStatus() {
+		this._status = (this._status) ? false : true
+	}
+	
+	// Get the correct SVG Icon
+	
+	get getCorrectSVGIcon() {
+		return this._status ? './assets/img/feather/eye.svg' : './assets/img/feather/eye-off.svg'
+	}
+	
 }
 
-function addBookToLibrary(name, author, pages, read) {
-	let newBook = new Book(name, author, pages, read)
+const addBook = (name, author, pages, status) => {
+	let book = new Book (name, author, pages, status)
+	books.push(book)
 }
 
-// EXAMPLES
+addBook('The Hobbit', 'J.R.R. Tolkien', 310, false)
+addBook('The Lord of The Rings', 'J.R.R. Tolkien', 1178, true)
+addBook('The Silmarillion', 'J.R.R. Tolkien', 365, false)
 
-addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 310, false)
-addBookToLibrary('The Lord of The Rings', 'J.R.R. Tolkien', 1178, true)
-addBookToLibrary('The Silmarillion', 'J.R.R. Tolkien', 365, false)
+// Helper Functions
+// Just a bunch of functions to make my job a little easier.
 
-// TABLE ROW: DeleteBtn Name Author Pages ReadBtn
-
-!function showBooks() {
-	const container = document.querySelector('tbody')
+const help = (function() {
 	
-	// Create each needed element by providing its text content,
-	// its parent and its type.
+	const containerTable = document.querySelector('tbody')
+	const containerForm = document.querySelector('.add-book')
 	
-	function createElement(str, parent, type = 'td') {
-		let el = document.createElement(type)
-				el.textContent = str
-		parent.appendChild(el)
-		
-		return el
+	const createElement = (content, parent, type = 'td') => {
+		let element = document.createElement(type)
+				element.textContent = content
+		parent.appendChild(element)
+		return element
 	}
 	
-	// Get the book's index in the books array.
-	function getBookIndex(name) {
-		return books.map((el) => el.name).indexOf(name)
-	}
-	
-	// Change the book's status in the array and in the table.
-	function changeReadStatus(bookName, btn, img) {
-		const index = getBookIndex(bookName)
-		const status = books[index].read
-		
-		books[index].changeStatus()
-	}
-	
-	// Remove the book from the table and from the array.
-	
-	function removeBook(name, row) {
-		const index = getBookIndex(name)
-		books.splice(index, index)
-		row.remove()
-	}
-	
-	// Create a row for each book.
-	// Each row needs a cell for every information about the book, a
-	// delete button and a change read status button.
-	
-	function addBooksToTable() {
-		books.forEach((book) => {
-			let bookRow = document.createElement('tr')
-					bookRow.classList.toggle('isBookRow')
-					
-			let bookDelete = createElement('', bookRow)
-			let bookName = createElement(book.name, bookRow)
-			let bookAuthor = createElement(book.author, bookRow)
-			let bookPages = createElement(book.pages, bookRow)
-			let bookRead = createElement('', bookRow)
-			
-			// Delete Book from table and array when clicked.
-			
-			let bookDeleteBtn = createElement('', bookDelete, 'button')
-					bookDeleteBtn.addEventListener('click', () => 
-																				 removeBook(book.name, bookRow))
-								
-			// Add a SVG Icon instead of text for both the delete button and the read one.
-			
-			let bookDeleteBtnImg = document.createElement('img')
-					bookDeleteBtnImg.setAttribute('src', './assets/img/feather/trash-2.svg')
-					bookDeleteBtnImg.setAttribute('alt', 'Delete Book')
-					
-			bookDeleteBtn.appendChild(bookDeleteBtnImg)
-			
-			let bookReadBtn = createElement('', bookRead, 'button')
-			
-			let bookReadBtnImg = document.createElement('img')
-					bookReadBtnImg.setAttribute('src', getCorrectSVGIcon(book.read))
-					bookReadBtnImg.setAttribute('alt', 'Change Book Read State')
-					
-			bookReadBtn.appendChild(bookReadBtnImg)
-					
-			// Change Book Read Status when clicked.
-			
-					bookReadBtn.addEventListener('click', () => {
-						changeReadStatus(book.name, bookReadBtn, bookReadBtnImg)
-						changeSVGIcon(bookReadBtnImg)
-					})
-					
-			// Get the correct SVG Icon.
-						
-			container.appendChild(bookRow)
-		})
-	}
-	
-	addBooksToTable()
-	
-	function getCorrectSVGIcon(status) {
-		return status ? './assets/img/feather/eye.svg' : './assets/img/feather/eye-off.svg'
-	}
-	
-	function changeSVGIcon(img) {
-		const imgSrc = img.getAttribute('src')
-		if (imgSrc == './assets/img/feather/eye.svg') {
+	const changeSVGIcon = (img) => {
+		const src = img.getAttribute('src')
+		if (src == './assets/img/feather/eye.svg') {
 			img.setAttribute('src', './assets/img/feather/eye-off.svg')
 		} else {
 			img.setAttribute('src', './assets/img/feather/eye.svg')
 		}
 	}
 	
-	// Function that updates the table each time a new book is added.
-	// It deletes each book rows and creates them anew.
+	// Get the book's index in the books array.
 	
-	function updateTable() {
-		const bookRows = container.querySelectorAll('.isBookRow')
-					bookRows.forEach((row) => row.remove())
-					
-		addBooksToTable()
+	const getBookIndex = (x) => {
+		return books.map((book) => book.name).indexOf(x)
 	}
 	
-	// Add a Book via the page.
-	// We need a input for each needed book information: name, author
-	// number of pages and read status.
-	
-	function addInput(str, id, name, parent, type = 'text') {
-		let label = document.createElement('label')
-				label.textContent = str
-				label.setAttribute('for', id)
-				
-		let input = document.createElement('input')
-				input.setAttribute('type', type)
-				input.setAttribute('name', name)
-				input.setAttribute('id', id)
+	return {
+		createElement,
+		changeSVGIcon,
+		getBookIndex,
+		containerTable,
+		containerForm
+	}
+		
+})()
 
-		let li = document.createElement('li')
-				li.appendChild(label)
-				li.appendChild(input)
+// We need to create a form for the user to add their books to the
+// library.
+
+const createForm = (function() {
+	const ul = document.querySelector('.add-book ul')
 	
+	const inputArr = []
+	
+	// A function to create multiple inputs and labels contained into
+	// a <li> element.
+	
+	const addInput = (content, id, name, parent, type = 'text') => {
+		const label = document.createElement('label')
+					label.setAttribute('for', id)
+					label.textContent = content
+					
+		const input = document.createElement('input')
+					input.setAttribute('id', id)
+					input.setAttribute('name', name)
+					input.setAttribute('type', type)
+		
+		const li = document.createElement('li')
+					li.appendChild(label)
+					li.appendChild(input)
+		
 		parent.appendChild(li)
+		inputArr.push(input)
 		return input
 	}
 	
-	!function addBookForm() {
-		const ulContainer = document.querySelector('.add-book')
-		const ul = ulContainer.querySelector('ul')
-		
-		const inputName = addInput('Name', 'book-name', 'book-name', ul)
-		const inputAuthor = addInput('Author', 'book-author', 'book-author', ul)
-		const inputPages = addInput('Number of Pages', 'book-pages', 'book-pages', ul)
-		const inputRead = addInput('Read Status', 'book-read', 'book-read', ul, 'checkbox')
-		
-		const inputArr = [inputName, inputAuthor, inputPages, inputRead]
-		
-		// Reset each input's value and unchecks the read status checkbox.
-		
-		function resetInput(arr = inputArr) {
-			arr.forEach((el) => {
-				if (el.getAttribute('type') == 'text') el.value = ''
-				if (el.getAttribute('type') == 'checkbox') el.checked = false
-			})
-		}
-		
-		inputName.addEventListener('change', () => console.log(inputName.value))
+	// A function that resets the inputs.
 	
-		// When the button is submitted, a new object is created and the
-		// updateTable() function is invoked.
-		
-		let submitButton = document.createElement('button')
-				submitButton.textContent = 'Submit'
-				submitButton.setAttribute('type', 'submit')
-				submitButton.addEventListener('click', () => {
-					addBookToLibrary(inputName.value, inputAuthor.value, inputPages.value, inputRead.checked)
-					updateTable()
-					resetInput()
+	const resetInput = () => {
+		inputArr.forEach((input) => {
+			if (input.getAttribute('type') == 'text') input.value = ''
+			if (input.getAttribute('type') == 'checkbox') input.checked = false
+		})
+	}
+	
+	// Inputs and Labels.
+	
+	const inputName = addInput('Name', 'book-name', 'book-name', ul)
+	const inputAuthor = addInput('Author', 'book-author', 'book-author', ul)
+	const inputPages = addInput('Pages', 'book-pages', 'book-pages', ul)
+	const inputRead = addInput('Read Status', 'book-read', 'book-read', ul, 'checkbox')
+	
+	// Submit Button.
+	
+	const createSubmitBtn = (function() {
+		const li = document.createElement('li')
+		const btn = help.createElement('Submit', li, 'button')
+					btn.setAttribute('type', 'submit')
+					btn.addEventListener('click', () => {
+						addBook(inputName.value, inputAuthor.value, inputPages.value, inputRead.checked)
+						console.log(books)
+						populateDOM.updateTable()
+						resetInput()
 				})
-				
-		let submitButtonLi = document.createElement('li')
-				submitButtonLi.appendChild(submitButton)
-				
-		ul.appendChild(submitButtonLi)
-	}()
-}()
+		help.containerForm.appendChild(li)
+	})()
+	
+})()
+
+const populateDOM = (function() {
+	
+	const populateTable = function() {
+		books.forEach((book) => {
+			
+			// Create a row for each book.
+			
+			const row = document.createElement('tr')
+						row.classList.toggle('isBookRow')
+			
+			const del = help.createElement('', row)
+			const name = help.createElement(book.name, row)
+			const author = help.createElement(book.author, row)
+			const pages = help.createElement(book.pages, row)
+			const status = help.createElement('', row)
+			
+			// Delete Button.
+			
+			const delBtn = help.createElement('', del, 'button')
+						delBtn.addEventListener('click', () => {
+							removeBook(book.name, row)
+						})
+						
+			const delImg = document.createElement('img')
+						delImg.setAttribute('src', './assets/img/feather/trash-2.svg')
+						delImg.setAttribute('alt', 'Delete Book')
+						
+			delBtn.appendChild(delImg)
+			
+			// Change Read Status Button.
+			
+			const statusBtn = help.createElement('', status, 'button')
+			const statusImg = document.createElement('img')
+						statusImg.setAttribute('src', book.getCorrectSVGIcon)
+						statusImg.setAttribute('alt', 'Change Book Read State')
+						
+			statusBtn.appendChild(statusImg)
+						
+			statusBtn.addEventListener('click', () => {
+				changeReadStatus(book.name)
+				help.changeSVGIcon(statusImg)
+			})
+			
+			// Append the row to the table.
+			
+			help.containerTable.appendChild(row)
+		})
+		
+	}
+	
+	populateTable()
+	
+	const changeReadStatus = (name) => {
+		const index = help.getBookIndex(name)
+		books[index].changeReadStatus
+	}
+	
+	const removeBook = (name, row) => {
+		const index = help.getBookIndex(name)
+		books.splice(index, index)
+		row.remove()
+	}
+	
+	const updateTable = () => {
+		const rows = help.containerTable.querySelectorAll('.isBookRow')
+					rows.forEach((row) => row.remove())
+		populateTable()
+	}
+	
+	return {
+		populateTable,
+		updateTable
+	}
+	
+})()
